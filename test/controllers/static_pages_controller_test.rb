48e36231dab0b5b89835957ca0231cb87e7cf1a3
Ben Freeman <djregn@gmail.com>
2015-11-13 20:01:47 -0600

    style and links added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Gathur"
+    assert_select "title", "Gathur"
   end
 
   test "should get help" do
@@ -17,6 +17,12 @@ class StaticPagesControllerTest < ActionController::TestCase
   	get :about
     assert_response :success
     assert_select "title", "About | Gathur"
+  end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Gathur"
   end
 
 end
