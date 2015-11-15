b8e985b288959ef0c9e56842394cd99a13798274
Ben Freeman <djregn@gmail.com>
2015-11-14 21:29:02 -0600

    Add login/logout functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114221112) do
+ActiveRecord::Schema.define(version: 20151115030417) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151114221112) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
